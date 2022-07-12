e5c115ad9709087ae59f996ba0f9b18cdba27941
Francesco Grazioso <40018163+FrancescoGrazioso@users.noreply.github.com>
2022-07-12 19:28:16 +0200

    Created navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import 'uikit/dist/css/uikit.css'
 import '../styles/globals.css'
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return <div>
-    //TODO aggiungere navbar
+
+    <div className="uk-fixed-navigation">
+      <nav className="uk-navbar">
+        <ul className="uk-navbar-nav">
+          <li className="uk-active"><Link href="/">Home</Link></li>
+          <li><Link href="/subscribe">Subscribe</Link></li>
+        </ul>
+      </nav>
+    </div>
+
+    <br/>
+    <br/>
+    <br/>
     <Component {...pageProps} />
   </div>
 }
